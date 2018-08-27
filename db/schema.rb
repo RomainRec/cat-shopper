feb3eb7cdbbdac4c0685e50bd90af991d384d0df
skageraz <skageraz@gmail.com>
2018-08-27 23:13:15 +0200

    bdd done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_164451) do
+ActiveRecord::Schema.define(version: 2018_08_27_211222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 2018_08_27_164451) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.bigint "users_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["users_id"], name: "index_orders_on_users_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -46,4 +53,5 @@ ActiveRecord::Schema.define(version: 2018_08_27_164451) do
   end
 
   add_foreign_key "carts", "users", column: "users_id"
+  add_foreign_key "orders", "users", column: "users_id"
 end
