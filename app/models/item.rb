0c4235fec38be9f0975f49593bf7b35ab3477dbb
skageraz <skageraz@gmail.com>
2018-08-27 23:09:33 +0200

    bdd done

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+  has_many :users, through: :carts
+  has_many :orders
+  has_many :carts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_154752) do
+ActiveRecord::Schema.define(version: 2018_08_27_164451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.bigint "users_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["users_id"], name: "index_carts_on_users_id"
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "title"
@@ -38,4 +45,5 @@ ActiveRecord::Schema.define(version: 2018_08_27_154752) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "carts", "users", column: "users_id"
 end
