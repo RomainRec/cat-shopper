46643ecc6d8a262b513242dc069dad4fd846aee0
Gaelle <gaelle.gorgori@gmail.com>
2018-08-29 13:50:05 +0200

    ajout params mailer

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,12 +1,17 @@
 class RegistrationController < ApplicationController
 
   def create
+    UserMailer.welcome_email("hugo.payet@live.fr").deliver_now!
+ # UserMailer.welcome_email(stripe_params["stripeEmail"]).deliver_now!
   @registration = Registration.new registration_params.merge(email: stripe_params["stripeEmail"],
                                                              card_token: stripe_params["stripeToken"])
   raise "Please, check registration errors" unless @registration.valid?
   @registration.process_payment
   @registration.save
+  #UserMailer.user_order(stripe_params["stripeEmail"]).deliver_now!
+  #UserMailer.welcome_email(stripe_params["stripeEmail"]).deliver_now!
   redirect_to @registration, notice: 'Registration was successfully created.'
+
 rescue e
   flash[:error] = e.message
   render :new
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,13 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
        @user = user
-     mail(to: @user.email, subject: "Petit message de bienvenue")
+     mail(from: "thpcatshopping@gmail.com", to: @user, subject: "Petit message de bienvenue")
   end
+
+  def user_order(user, amount)
+    @user = user
+    @amount = amount
+     mail(from: "thpcatshopping@gmail.com", to: @user, subject: "Votre commande")
+   end
 
 end
