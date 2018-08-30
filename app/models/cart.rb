7ebd1842e15c2594ca3d6eca4e44591e45a407a5
Titouax <titouan.dessus@gmail.com>
2018-08-30 08:10:55 +0200

    fin du tuto

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,4 +3,14 @@ class Cart < ApplicationRecord
   has_many :orders
   has_many :cart_items, dependent: :destroy
 
+  def add_product(item)
+    product = cart_items.find_by(item: item)
+
+    if product
+      product.quantity += 1
+    else
+      product = cart_items.new(item: item)
+    end
+    product
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,4 @@
 class Item < ApplicationRecord
-  
   has_many :orders
   has_many :cart_items, dependent: :nullify
 end
