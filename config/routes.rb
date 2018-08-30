a4b94cfe0b926ed04f306ad2c777419ff6112eda
Titouax <titouan.dessus@gmail.com>
2018-08-30 17:47:46 +0200

    some change

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
   resources :line_items
-  resources :cart_items
   devise_for :users, controllers: {sessions: 'users/sessions'}
   root 'pages#index'
   get 'pages/presentation', to: 'pages#presentation'
   get 'items/index', to: 'items#index'
   resources :items
-  get 'carts/index', to: 'carts#index'
+
   resources :carts
-  get 'orders/index', to: 'orders#index'
-  resources :orders
+
   resources :charges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
